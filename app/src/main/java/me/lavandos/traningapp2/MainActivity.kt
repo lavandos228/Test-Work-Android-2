1ed497292229c113bb32d9dcf15e953433c8e4fd
lavandos228 <vlad.babich.vlad@mail.ru>
2021-05-08 14:43:56 +0300

    commit ...

---
+++ app/src/main/java/me/lavandos/traningapp2/MainActivity.kt
@@ -0,0 +1,22 @@
+package me.lavandos.traningapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //val mainText = findViewById<TextView>(R.id.mainText)
+        //mainText.text = "LALKA"
+        //mainText.text = "Lalka"
+
+
+
+    }
+
+}
